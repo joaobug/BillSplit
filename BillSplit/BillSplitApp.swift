37f4bafa1da2e73a98d3be84d016bc1a10d2f2ff
joaobug <joaoenfrance@gmail.com>
2024-02-09 17:48:29 -0300

    Initial Commit

---
+++ BillSplit/BillSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitApp.swift
+//  BillSplit
+//
+//  Created by Joao Leal on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
